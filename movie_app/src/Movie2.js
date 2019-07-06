8c7638e637b942f9c49a96e8eec59814f30e2ad7
junblee <topguide82@gmail.com>
2019-07-06 23:45:46 +0900

    2

---
+++ movie_app/src/Movie2.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import './Movie.css';
+
+class Movie extends Component{ 
+    render() { 
+        return (
+            <div class Name="Movie_class">
+                <MoviePoster poster = {this}>
+            </div>
+        )
+    }
+}
